e04069e3e8ab9fc821052451b7ffd5c5a6e62be9
Kirnic21 <marcelokirnic@gmail.com>
2023-10-14 20:43:09 -0300

    ddo quantity functionality

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,49 @@
 import { useEffect,useState } from "react"
 const Card = (props)=>{
+    let [quantity,setQuantity] = useState(0)
+    const [showForm,setShowForm] = useState(false)
+    const showFormToggle = () => {
+        setShowForm(!showForm);
+      }
+    const decreaseQuantity = (e)=>{
+        e.preventDefault();
+        if(quantity > 0)
+        {
+        quantity--;
+        setQuantity(quantity);
+        }
+    }
+    const increaseQuantity = (e)=>{
+        e.preventDefault();
+        quantity++;
+        setQuantity(quantity);
+    }
+    const handleSubmit = (e)=>{
+        
+        e.preventDefault()
+            
+    }
+    const handleInputChange = (e)=>{
+        
+        e.preventDefault()
+        quantity = e.target.value;
+        setQuantity(quantity)
+    }
     return(
         <div className = "card">
         <h1 className ="title">{props.title}</h1>
         <img className ="image" src = {props.image}></img>
+        <h3 className="price">U$ {props.price}</h3>
+        
+            <button id = {props.id} onClick={decreaseQuantity}>-</button>
+                <button onClick={showFormToggle}>{quantity}</button>
+                {showForm && (
+                <form onSubmit ={handleSubmit}>
+                    <input onChange={handleInputChange} type="number" defaultValue={quantity} ></input>
+                </form>
+                )}         
+            <button id = {props.id} onClick={increaseQuantity}>+</button>
+    
         </div>
     )
 }
@@ -13,6 +53,7 @@ const Cards = ()=>{
         const fetchData = async ()=>{
             const data = await fetch('https://fakestoreapi.com/products?limit=10')
             const dataJson = await data.json();
+            console.log(dataJson)
             setItem(dataJson)
         }
         fetchData()
@@ -22,7 +63,10 @@ const Cards = ()=>{
                 {item.map((items)=>{
                     return (<Card
                     title={items.title}
-                    image={items.image}>
+                    image={items.image}
+                    price={items.price}
+                    key = {items.id}>
+                    
                     </Card>)
                 })}
                 
